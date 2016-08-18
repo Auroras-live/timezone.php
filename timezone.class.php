600a62b4b410c84df4fbe32ceceb9c3db2c2f5cd
David Gray <grayda@solidinc.org>
2016-08-18 15:19:39 +1000

    Fixed timezone again, and added comments
    Offset should now be applied correctly.

--- timezone.class.php
+++ timezone.class.php
@@ -5,28 +5,41 @@
     public $offset, $offsetFormat;
 
     function __construct($date = "now", $offset = null) {
+      $this->offset = $offset;
       if(date_create($date) === false) {
         return false;
       } else {
-        if(is_null($offset)) {
+        // If we haven't explicitly set an offset
+        if(is_null($this->offset)) {
+          // If we have set a timezone via $_REQUEST
           if(isset($_REQUEST["tz"])) {
+            // Set our offset, to whatever we $_REQUESTed
             $this->offset = $_REQUEST["tz"];
+            // Return the difference between UTC and the offset, as a + or - value (e.g. -600 = +10:00)
             $this->offsetFormat = (new \DateTime($date, new \DateTimeZone("UTC")))->diff((new \DateTime($date, new \DateTimeZone("UTC")))->sub(\DateInterval::createFromDateString($this->offset . " minutes")))->format("%R%H:%I");
+          // If we haven't set a timezone, and timezone is null,
           } else {
+            // Set offset to 0 and be done with it
             $this->offsetFormat = "+00:00";
             $this->offset = 0;
           }
+        // If we have set an offset via $offset,
         } else {
+          // Return the difference between UTC and the offset, same as above.
           $this->offsetFormat = (new \DateTime($date, new \DateTimeZone("UTC")))->diff((new \DateTime($date, new \DateTimeZone("UTC")))->sub(\DateInterval::createFromDateString($this->offset . " minutes")));
           $this->offsetFormat = $this->offsetFormat->format("%R%H:%I");
         }
-
+        // If the offset in $date does NOT match the offset we're passing
         if(((new \DateTime($date))->getOffset() / 60) !== $this->offset) {
+          // Assume offset hasn't been added / subtracted, so modify the date
           $date = (new \DateTime($date, new \DateTimeZone("UTC")))->sub(\DateInterval::createFromDateString($this->offset . " minutes"));
+        // If the offset DOES match
         } else {
+          // Then assume the offset has been applied
           $date = (new \DateTime($date, new \DateTimeZone("UTC")));
         }
 
+        // Finally, construct the date, append our timezone offset to it, then pass it up to our parent class
         parent::__construct($date->format("Y-m-d\TH:i:s") . $this->offsetFormat, null);
       }
     }
